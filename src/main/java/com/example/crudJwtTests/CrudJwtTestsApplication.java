0bd1d2ac6b4069436031340c0eb4ac09a88cc4c2
kelwinferreirazup <kelwin.ferreira@zup.com.br>
2020-07-24 20:00:38 -0300

    Started project

---
+++ src/main/java/com/example/crudJwtTests/CrudJwtTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.crudJwtTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudJwtTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudJwtTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crudJwtTests/CrudJwtTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudJwtTests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudJwtTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
